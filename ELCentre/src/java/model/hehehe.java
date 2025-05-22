20595b841baec2efa65bffe56a35a7a87d8ffdb2
chur04 <vuvanchu3012@gmail.com>
2025-05-22 19:12:33 +0700

    codehomnay

---
+++ ELCentre/src/java/model/hehehe.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author wrx_Chur04
+ */
+public class hehehe {
+    private int hehehe ; 
+}
